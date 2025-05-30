e79dabc458775e213e12afb7ae21d385bb6cf0aa
Gui.dev <141243910+Guilherme-SB-DEV@users.noreply.github.com>
2025-05-30 12:09:49 +0000

    banco

---
+++ mysql/banco.sql
@@ -0,0 +1,21 @@
+/* Lógico_2: */
+
+CREATE TABLE usuario (
+    id INT PRIMARY KEY,
+    nome VARCHAR(100),
+    email VARCHAR(100)
+);
+
+CREATE TABLE tarefa (
+    id INT PRIMARY KEY,
+    descricao VARCHAR(100),
+    setor VARCHAR(100),
+    status VARCHAR(100),
+    prioridade VARCHAR(100),
+    fk_usuario_id INT
+);
+ 
+ALTER TABLE tarefa ADD CONSTRAINT FK_tarefa_2
+    FOREIGN KEY (fk_usuario_id)
+    REFERENCES usuario (id)
+    ON DELETE CASCADE;
